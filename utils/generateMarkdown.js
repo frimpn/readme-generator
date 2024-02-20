73784caea73f97751e5b6eb3bfbfbc9fc61da408
Nana <frimpongnana8@gmail.com>
2024-02-20 12:16:31 +0000

    used a template literal to create the body of the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,54 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  1.[Description](#description)
+  2.[Installation](#installation)
+  3.[Usage](#usage)
+  4.[Credits](#credits)
+  5.[License](#license)
+  6.[contributing](#contributing)
+  7.[Tests](#tests)
+  8.[Questions](#questions)
+
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
+
+https://img.shields.io/badge/license-${data.license}-blue
+
+
+
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+Github id: ${data.github}
+Email: ${data.email}
+
+
 `;
 }
 
